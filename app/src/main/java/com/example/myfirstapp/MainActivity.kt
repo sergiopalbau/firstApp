fce637f7533ac7df94a26e81d0ded96c4d5e217e
sergio <s.palomo.b@gmail.com>
2022-06-02 21:43:33 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.myfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val btn = findViewById<Button>(R.id.button)
+        btn.setOnClickListener {
+            Toast.makeText(this, "Has pulsado el botón", Toast.LENGTH_LONG).show()
+        }
+    }
+}
